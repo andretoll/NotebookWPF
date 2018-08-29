a45fda29e15bb5becbac1d9e96404bb4833e67ba
André Toll <andretoll@outlook.com>
2018-08-29 02:02:23 +0200

    - Defined a new toggle button - Started creating toolbar for text editor

--- NotebookWPF/View/MainWindow.xaml.cs
+++ NotebookWPF/View/MainWindow.xaml.cs
@@ -49,6 +49,8 @@ namespace NotebookWPF
             this.DataContext = _notebookViewModel;
 
             HomeRadioButton.IsChecked = true;
+
+            InitiateToolbarValues();
         }
 
         #endregion
@@ -120,7 +122,12 @@ namespace NotebookWPF
         /// <param name="e"></param>
         private void NotesListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            NoteTextEditor.Focus();
+            if (NotesListBox.SelectedItem != null)
+            {                
+                NoteTextEditor.Focus();
+
+                UpdateToolbarValues();
+            }
         }
 
         /// <summary>
@@ -130,7 +137,12 @@ namespace NotebookWPF
         /// <param name="e"></param>
         private void FavoriteNotesListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            NoteTextEditor.Focus();
+            if (FavoriteNotesListBox.SelectedItem != null)
+            {
+                NoteTextEditor.Focus();
+
+                UpdateToolbarValues();
+            }
         }           
 
         /// <summary>
@@ -196,6 +208,86 @@ namespace NotebookWPF
             }
         }
 
+        /// <summary>
+        /// On Note text editor selection changed
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void NoteTextEditor_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+            UpdateToolbarValues();
+        }
+
+        #region Text Editor Events
+
+        /// <summary>
+        /// On chaning Font Family
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void FontFamilyComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var fontFamily = (sender as ComboBox).SelectedItem;
+
+            NoteTextEditor.Selection.ApplyPropertyValue(FontFamilyProperty, fontFamily);
+
+            NoteTextEditor.Focus();
+        }
+
+        /// <summary>
+        /// On changing font size
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void FontSizeComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var fontSize = (sender as ComboBox).SelectedItem;
+
+            NoteTextEditor.Selection.ApplyPropertyValue(FontSizeProperty, fontSize);
+
+            NoteTextEditor.Focus();
+        }
+
+        /// <summary>
+        /// On changing bold text
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BoldToggleButton_Click(object sender, RoutedEventArgs e)
+        {
+            if ((sender as ToggleButton).IsChecked ?? true)
+            {
+                NoteTextEditor.Selection.ApplyPropertyValue(FontWeightProperty, FontWeights.Bold);
+            }
+            else
+            {
+                NoteTextEditor.Selection.ApplyPropertyValue(FontWeightProperty, FontWeights.Normal);
+            }
+
+            NoteTextEditor.Focus();
+        }
+
+        /// <summary>
+        /// On chaning italic text
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ItalicToggleButton_Click(object sender, RoutedEventArgs e)
+        {
+            if ((sender as ToggleButton).IsChecked ?? true)
+            {
+                NoteTextEditor.Selection.ApplyPropertyValue(FontStyleProperty, FontStyles.Italic);
+            }
+            else
+            {
+                NoteTextEditor.Selection.ApplyPropertyValue(FontStyleProperty, FontStyles.Normal);
+            }
+
+            NoteTextEditor.Focus();
+        }
+
+        #endregion
+
         #endregion
 
         #region Helper Methods
@@ -212,6 +304,111 @@ namespace NotebookWPF
             // If Storyboard was found, play it
             if (sb != null)
                 ((Storyboard)sb).Begin(pnl);
+        }
+
+        /// <summary>
+        /// Initiate Text editor toolbar values
+        /// </summary>
+        private void InitiateToolbarValues()
+        {
+            List<double> fontSizes = new List<double>();
+            fontSizes.Add(3);
+            fontSizes.Add(4);
+            fontSizes.Add(5);
+            fontSizes.Add(6);
+            fontSizes.Add(6.5);
+            fontSizes.Add(7);
+            fontSizes.Add(7.5);
+            fontSizes.Add(8);
+            fontSizes.Add(8.5);
+            fontSizes.Add(9);
+            fontSizes.Add(9.5);
+            fontSizes.Add(10);
+            fontSizes.Add(10.5);
+            fontSizes.Add(11.5);
+            fontSizes.Add(12);
+            fontSizes.Add(12.5);
+            fontSizes.Add(13.5);
+            fontSizes.Add(14);
+            fontSizes.Add(15);
+            fontSizes.Add(16);
+            fontSizes.Add(17);
+            fontSizes.Add(18);
+            fontSizes.Add(19);
+            fontSizes.Add(20);
+            fontSizes.Add(22);
+            fontSizes.Add(24);
+            fontSizes.Add(26);
+            fontSizes.Add(28);
+            fontSizes.Add(30);
+            fontSizes.Add(32);
+            fontSizes.Add(34);
+            fontSizes.Add(36);
+            fontSizes.Add(38);
+            fontSizes.Add(40);
+            fontSizes.Add(44);
+            fontSizes.Add(48);
+            fontSizes.Add(52);
+            fontSizes.Add(56);
+            fontSizes.Add(60);
+            fontSizes.Add(64);
+            fontSizes.Add(68);
+            fontSizes.Add(72);
+            fontSizes.Add(76);
+            fontSizes.Add(80);
+            fontSizes.Add(88);
+            fontSizes.Add(96);
+            fontSizes.Add(104);
+            fontSizes.Add(112);
+            fontSizes.Add(120);
+            fontSizes.Add(128);
+            fontSizes.Add(136);
+            fontSizes.Add(144);
+
+            FontSizeComboBox.ItemsSource = fontSizes;
+        }
+
+        /// <summary>
+        /// Update Text editor toolbar values
+        /// </summary>
+        private void UpdateToolbarValues()
+        {
+            try
+            {
+                // Set FontFamily
+                var fontFamily = NoteTextEditor.Selection.GetPropertyValue(TextElement.FontFamilyProperty);
+                FontFamilyComboBox.SelectedItem = fontFamily;
+
+                // Set FontSize
+                var fontSize = NoteTextEditor.Selection.GetPropertyValue(TextElement.FontSizeProperty);
+                foreach (var item in FontSizeComboBox.Items)
+                {
+                    if ((double)item == (double)fontSize)
+                    {
+                        FontSizeComboBox.SelectedItem = item;
+                        break;
+                    }
+
+                    FontSizeComboBox.SelectedIndex = 15;
+                }
+
+                // Set Bold
+                var isBold = NoteTextEditor.Selection.GetPropertyValue(FontWeightProperty);
+                if ((FontWeight)isBold == FontWeights.Bold)
+                {
+                    BoldToggleButton.IsChecked = true;
+                }
+                else BoldToggleButton.IsChecked = false;
+
+                // Set Italic
+                var isItalic = NoteTextEditor.Selection.GetPropertyValue(FontStyleProperty);
+                if ((FontStyle)isItalic == FontStyles.Italic)
+                {
+                    ItalicToggleButton.IsChecked = true;
+                }
+                else ItalicToggleButton.IsChecked = false;
+            }
+            catch { }
         }
 
         #endregion        
